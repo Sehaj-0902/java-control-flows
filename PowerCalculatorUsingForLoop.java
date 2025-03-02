8070e131c69da88473344d7c34c40c41956c48e8
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-03 01:38:15 +0530

    [Sehaj] ADD: Program to find the power of a number using for loop

--- PowerCalculatorUsingForLoop.java
+++ PowerCalculatorUsingForLoop.java
@@ -1,4 +1,4 @@
-// Program to find the power of a number
+// Program to find the power of a number using for loop
 
 // Import Scanner class from java.util package to take user input
 import java.util.Scanner;
